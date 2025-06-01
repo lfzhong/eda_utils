370ac75b995a779216ff51b3d5cce81d33177a5c
lfzhong <48391285+lfzhong@users.noreply.github.com>
2025-06-01 11:28:51 -0400

    Create __init__.py

---
+++ eda_utils/__init__.py
@@ -0,0 +1,9 @@
+from .univariate import run_univariate_analysis
+from .bivariate import run_bivariate_analysis
+from .combined import plot_univariate_bivariate
+
+__all__ = [
+    "run_univariate_analysis",
+    "run_bivariate_analysis",
+    "plot_univariate_bivariate"
+]
