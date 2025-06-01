9cdb17022d292c1c3808a4cdcbfe75a864337146
lfzhong <48391285+lfzhong@users.noreply.github.com>
2025-06-01 11:45:19 -0400

    Update __init__.py

--- eda_utils/__init__.py
+++ eda_utils/__init__.py
@@ -1,9 +1,11 @@
 from .univariate import run_univariate_analysis
 from .bivariate import run_bivariate_analysis
 from .combined import plot_univariate_bivariate
+from .correlation import compute_spark_correlation_matrix
 
 __all__ = [
     "run_univariate_analysis",
     "run_bivariate_analysis",
-    "plot_univariate_bivariate"
+    "plot_univariate_bivariate",
+    "compute_spark_correlation_matrix"
 ]
